ad78e251cb34c3a42b88091353ed3374a650a065
Dan Fornika <dfornika@gmail.com>
2023-08-02 15:07:50 -0700

    Revert "Update"
    This reverts commit 04c2788bd1cbf1764c1a76a66f6adc5869559520.

--- scripts/create-deployment.sh
+++ scripts/create-deployment.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rm -r target/deploy/routine-nanopore-qc 2> /dev/null
-mkdir -p target/deploy/routine-nanopore-qc
-mkdir -p target/deploy/routine-nanopore-qc/js
-cp -r resources/public/css target/deploy/routine-nanopore-qc
-cp -r resources/public/images target/deploy/routine-nanopore-qc
-cp target/public/cljs-out/prod/main_bundle.js target/deploy/routine-nanopore-qc/js/main.js
-cp resources/public/index_prod.html target/deploy/routine-nanopore-qc/index.html
+rm -r target/deploy/routine-sequence-qc 2> /dev/null
+mkdir -p target/deploy/routine-sequence-qc
+mkdir -p target/deploy/routine-sequence-qc/js
+cp -r resources/public/css target/deploy/routine-sequence-qc
+cp -r resources/public/images target/deploy/routine-sequence-qc
+cp target/public/cljs-out/prod/main_bundle.js target/deploy/routine-sequence-qc/js/main.js
+cp resources/public/index_prod.html target/deploy/routine-sequence-qc/index.html
 pushd target/deploy > /dev/null
-tar -czf routine-nanopore-qc.tar.gz routine-nanopore-qc
-rm -r routine-nanopore-qc
+tar -czf routine-sequence-qc.tar.gz routine-sequence-qc
+rm -r routine-sequence-qc
 popd > /dev/null
